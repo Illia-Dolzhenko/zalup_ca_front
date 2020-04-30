43c17afa57e172d6037fd80aa23c86e3c87e17bf
Illia Dolzhenko <Illia_Dolzhenko@epam.com>
2020-05-01 01:17:38 +0300

    1

--- main/js/base.js
+++ main/js/base.js
@@ -1,11 +1,12 @@
+const GET_POSTS_URL = "http://localhost:8080/getPosts";
+const GET_POST_URL = "http://localhost:8080/getPost";
+const CREATE_POST_URL = "http://localhost:8080/createPost";
+const ADD_COMMENT_URL = "http://localhost:8080/addComment";
+const REGISTER_URL = "http://localhost:8080/register";
+const REGISTER_PAGE = "register.html";
+
 function sendGet(url, success) {
-
-    const user = localStorage.getItem("user");
-
-    if(user == null){
-        window.location.href="register.html";
-        return;
-    }
+    const user = getUser();
 
     $.ajax
     ({
@@ -21,11 +22,44 @@ function sendGet(url, success) {
         },
         error: function (xhr) {
             console.log(xhr.status);
-            if(xhr.status === 401){
-                window.location.href="register.html";
+            if (xhr.status === 401) {
+                window.location.href = REGISTER_PAGE;
             }
         }
     });
+}
+
+function sendPost(url, dataToSend, success) {
+    const user = getUser()
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        dataType: 'json',
+        contentType: 'application/json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + user);
+        },
+        data: JSON.stringify(dataToSend),
+        processData: false,
+        success: function (data) {
+            success(data);
+        },
+        error: function (xhr) {
+            if (xhr.status === 401) {
+                window.location.href = REGISTER_PAGE;
+            }
+        }
+    });
+}
+
+function getUser() {
+    const user = localStorage.getItem("user");
+    if (user == null) {
+        window.location.href = REGISTER_PAGE;
+        return null;
+    }
+    return user;
 }
 
 function register(url, user, response) {
